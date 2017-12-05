4174ede864cbc21bf41159b0a83ebae558dcc7ed
han1193569188 <1193569188@qq.com>
2017-12-05 18:45:34 +0800

    news

---
+++ js/details.js
@@ -0,0 +1,11 @@
+$(function () {
+   let obj=JSON.parse(sessionStorage.media);
+    $("h1").html(obj.title);
+    $("#from").html(obj.src);
+    $("#time").html(obj.time);
+    $("article").html(obj.content);
+    $(".back").click(function (e) {
+        e.preventDefault();
+        history.back();
+    })
+});
